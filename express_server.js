e9ff6c1d867f39ce71af14a73971cb3543441e83
iliacodes <6ac177ed@protonmail.com>
2022-12-16 18:01:15 -0800

    adding final changes to project. checking if user exists or not for /DELETE and /urls/:id

--- express_server.js
+++ express_server.js
@@ -4,7 +4,9 @@ const bcrypt = require("bcryptjs")
 const app = express();
 const { generateRandomString, getUsersByEmail, userURL } = require('./helper.js');
 
+
 const PORT = 8080; // default port 8080
+
 
 // MIDDLEWARE
 app.set("view engine", "ejs");
@@ -25,6 +27,7 @@ const users = {
   }
 };
 
+
 // Database for storing userURL's.
 // Example of Object Structure within urlDatabase. 
 const urlDatabase = {
@@ -33,6 +36,7 @@ const urlDatabase = {
     userID: "testUser"
   }
 };
+
 
 // If user is logged in, '/' redirects to '/urls'. If user is not logged in, '/' redirects to '/login' page.
 app.get("/", (req, res) => {
@@ -46,11 +50,12 @@ app.get("/", (req, res) => {
   }
 });
 
+
 // Page to display list of current user's URL's.
 // If user is not logged in, redirect to html response.
 app.get("/urls", (req, res) => {
   const userID = req.session.user_id;
-  const user = users[userID]
+  const user = users[userID];
   if (!user) {
     res.send("You need to <a href='/login'>login</a> to create shortened urls.")
   } else {
@@ -59,10 +64,12 @@ app.get("/urls", (req, res) => {
       userID,
       urls, 
       user
-    }; 
+    };
+
     res.render("urls_index", templateVars);
   };
 });
+
 
 // Page renders urls_new page. Only logged in users can create tinyURL
 // Checks for logged in user, if none, redirect to login page.
@@ -70,15 +77,17 @@ app.get("/urls/new", (req, res) => {
   let userID = req.session.user_id;
   const user = users[userID];
   if (!user) {
-    res.send("Please <a href='/login'>login</a> to create new Tiny Urls.")
+    res.redirect('/login')
   } else {
     const templateVars = { 
       userID: userID,
       user
     }
+
     res.render("urls_new", templateVars);
   }
 });
+
 
 // Page to redirect short URL to corresponding long URL. Checks to see if short URL exists, throws a relevant error message if it does not.
 app.get("/u/:id", (req, res) => {
@@ -91,6 +100,7 @@ app.get("/u/:id", (req, res) => {
   };
 });
 
+
 // Page to display specific URL with redirection to specfic URL implemented. Allows for tinyURL editing if it belongs to user. If it does not belong to user, throws relevant error message. If it does not exist, throws relevant error message
 app.get("/urls/:id", (req, res) => {
   let userID = req.session.user_id;
@@ -100,6 +110,7 @@ app.get("/urls/:id", (req, res) => {
     return res.send("You need to <a href='/login'>login</a> to edit your urls. If you are logged in, this URL is not yours or does not exist.")
   } 
   const urlsForuser = userURL(userID, urlDatabase);
+
   if (urlsForuser[id]) {
   const longURL = urlDatabase[id].longURL;
   const templateVars = { 
@@ -107,12 +118,26 @@ app.get("/urls/:id", (req, res) => {
       longURL,
       userID, 
       user
-    } 
+    }
     res.render("urls_show", templateVars);
   } else {
     res.send('Url does not belong to this user. Please login with correct User to edit this url.');
   }
 });
+
+
+// Renders the urls_login page. If the user is already logged in, it redirects to urls page.
+app.get("/login", (req, res) => {
+  let userID = req.session.user_id;
+  const user = users[userID];
+
+  if (user) {
+    res.redirect('/urls');
+  } else {
+    res.render("urls_login", {user: null});
+  };
+});
+
 
 // For rendering /register page. Redirects if logged in, if not, continues to render page to allow for user registration.
 app.get("/register", (req, res) => {
@@ -127,9 +152,11 @@ app.get("/register", (req, res) => {
         userID, 
         longURL: urlDatabase[userID]
       };
+
       res.render("register", templateVars);
     };
 });
+
 
 // Creation a new tinyURL. Checks to see if user is logged-in to do this. Redirects user to /urls page with newly implemented url on display from the user's database.
 app.post("/urls/new", (req, res) => {
@@ -150,25 +177,28 @@ app.post("/urls/new", (req, res) => {
   };
 });
 
+
 // Button post to allow for deletion of personal URL from the urlDatabase.
 app.post("/urls/:id/delete", (req, res) => {
   let userID = req.session.user_id;
-  const user = users[userID];
-  if (!user) {
-    res.redirect("/login")
-  } else {
-    const id = req.params.id;
-    delete urlDatabase[id];
-  }
+  const shortURL = urlDatabase[req.params.id];
+
+  if (userID !== shortURL.userID) {
+    return res.redirect("/login");
+  } 
+
+  delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
+
 
 // Allows for editing of the url. Checks if user owns url. Sends an error if the url doesn't include http or https.
 app.post("/urls/:id", (req, res) => {
   const userID = req.session.user_id;
   const user = users[userID];
   const longURL = req.body.longURL;
-  if (!user) {
+
+  if (user !== urlDatabase[req.params.id]) {
     res.redirect('/login')
   } else if (longURL.startsWith('http://') || longURL.startsWith('https://')) {
     const id = req.params.id;
@@ -189,29 +219,21 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-// Renders the urls_login page. If the user is already logged in, it redirects to urls page.
-app.get("/login", (req, res) => {
-  let userID = req.session.user_id;
-  const user = users[userID];
-  if (user) {
-    res.redirect('/urls');
-  } else {
-    res.render("urls_login", {user: null});
-  };
-});
 
 // Allows user to login if user in in the databse. Redirects logged in user to urls page
 app.post('/login', (req,res) => {
   const email = req.body.email;
   const password = req.body.password;
   const user = getUsersByEmail(users, email);
+
   if (!user || !bcrypt.compareSync(password, user.password)) {
-    res.send("Email has already been used. Or account does not exist. Try Again. <a href='/login'>Login here</a>");
+    res.status(403).send("Username or password do not match. Try Again. <a href='/login'>Login here</a>");
   } else {
     req.session.user_id = user.id;
     res.redirect('/urls');
   };
 });
+
 
 // Allows creation of new user and pushes that user to the userdatabase.
 // If user exists, throws an error. if no user or pass was implemented, throw an error.
@@ -220,9 +242,11 @@ app.post("/register", (req, res) => {
   const newUserEmail = req.body.email;
   const newUserPass = req.body.password;
   const hashPassword = bcrypt.hashSync(newUserPass);
+
   if (!newUserEmail || !newUserPass) {
     return res.status(404).send("Please enter registration credentials. Please <a href='/register'> try again.</a>");
   };
+
   if (getUsersByEmail(users, newUserEmail)) {
     res.status(404).send("User has already been established. Please register with a different email. <a href='/register'>Regsiter</a>");
   } else {
@@ -237,6 +261,7 @@ app.post("/register", (req, res) => {
     res.redirect("/urls");
   };
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
